d2f9962ee00f3deb2c3d0e40198e6753ea491f90
SartyGirho <46842786+SartyGirho@users.noreply.github.com>
2019-06-02 12:56:21 +0530

    Add files via upload
    An Armstrong number of three digits is an integer such that the sum of the cubes of its digits is equal to the number itself. For example, 371 is an Armstrong number since 3**3 + 7**3 + 1**3 = 371.

---
+++ Armstrongnumbers.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,x,y,z,s;
+
+    cout<<"DETERMINE IF THE THREE DIGIT NUMBER IS ARMSTRONG OR NOT"<<endl;
+    cout<<"Enter the THREE digit number you wish to check"<<endl;
+    cin>>n;
+
+    if(n>=100 && n<=999)
+    {
+        cout<<"NUMBER IN RANGE"<<endl;
+
+        x=n%10;
+        y=n/10;
+        z=y%10;
+        s=y/10;
+
+        if(n==(x*x*x)+(z*z*z)+(s*s*s))
+        {
+            cout<<"NUMBER IS AN ARMSTRONG"<<endl;
+        }
+        else
+        {
+            cout<<"NUMBER IS NOT AN ARMSTRONG"<<endl;
+        }
+    }
+    else
+    {
+        cout<<"NUMBER OUT OF RANGE"<<endl;
+    }
+
+    return 0;
+
+}
+
+
